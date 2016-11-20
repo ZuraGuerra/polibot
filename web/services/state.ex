9c53220774c4df46dc1c4ae99b1c1784ccc1b2f9
Zura Guerra <guerrazuramx@gmail.com>
2016-11-20 12:46:00 -0600

    Add country id on state generation

--- web/services/state.ex
+++ web/services/state.ex
@@ -10,10 +10,10 @@ defmodule Polibot.StateServices do
     end
   end
 
-  def generate_info do
+  def generate_info(country_id) do
     import Polibot.InfoServices
 
-    %{}
+    %{country_id: country_id}
     |> Map.put(:name, assign_random(:state_name))
     |> Map.put(:avg_salary, generate_random_number(3))
     |> Map.put(:human_development, generate_random_percentage(40, 80))
