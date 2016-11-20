cdde16a15217bb82023a6117ddadf0c036478568
Zura Guerra <guerrazuramx@gmail.com>
2016-11-20 01:58:01 -0600

    Send background story with buttons to candidate

--- web/controllers/chat_controller.ex
+++ web/controllers/chat_controller.ex
@@ -7,13 +7,19 @@ defmodule Polibot.ChatController do
   def chat(conn, %{"entry" => [%{"messaging" => [%{"postback" => %{"payload" => "Let's run for presidency!"},
                                                    "recipient" => %{"id" => page_id},
                                                    "sender" => %{"id" => user_id}}|_]}|_]}) do
-
     candidate = CandidateServices.create!(user_id)
     avatar_url = CandidateServices.get_avatar(candidate)
 
+    # Send avatar
     avatar_message = MessageServices.image(candidate.fb_id, avatar_url) |> Poison.encode!
     HTTPotion.post!(@messages_url, [body: avatar_message, status_code: 200,
                                    headers: ["Content-Type": "application/json"]])
+    # Send background story
+    background_story = CandidateServices.generate_story(candidate)
+    buttons = [MessageServices.postback_button("View my presidential stats", "I'd like to view my presidential stats")]
+    background_message = MessageServices.button_template(candidate.fb_id, background_story, buttons)
+    HTTPotion.post!(@messages_url, [body: background_message, status_code: 200,
+                                    headers: ["Content-Type": "application/json"]])
     render conn, "fb_callback.json"
   end
 
--- web/services/message.ex
+++ web/services/message.ex
@@ -14,4 +14,38 @@ defmodule Polibot.MessageServices do
       }
     }
   end
+
+  def button_template(recipient_id, text, buttons) do
+    %{
+      "recipient" => %{
+        "id" => recipient_id
+      },
+      "message" => %{
+        "attachment" => %{
+          "type" => "template",
+          "payload" => %{
+            "template_type" => "button",
+            "text" => text,
+            "buttons" => buttons
+          }
+        }
+      }
+    }
+  end
+
+  def url_button(url, title) do
+    %{
+      "type" => "web_url",
+      "url" => url,
+      "title" => title
+    }
+  end
+
+  def postback_button(payload, title) do
+    %{
+      "type" => "postback",
+      "title" => title,
+      "payload" => payload
+    }
+  end
 end
